c08bdd63d9ef34795a82342bb0d541c23df1310e
Ruairidh Carmichael <ruairidhcarmichael@live.co.uk>
2015-01-26 17:22:54 +0000

    Initial Commit.

---
+++ themes/monokai.lua
@@ -0,0 +1,12 @@
+theme = {
+	
+	panelbg = {39, 40, 34},
+	text = {255, 255, 255},
+	number = {174, 129, 255},
+	string = {230, 219, 116},
+	boolean_true = {174, 129, 255},
+	boolean_false = {174, 129, 255}
+
+}
+
+return theme
